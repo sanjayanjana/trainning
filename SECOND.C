414401595130dad1a5c78ac7bd9345a0e67fedd4
sanjayanjana <93249310+sanjayanjana@users.noreply.github.com>
2021-10-27 15:07:34 +0530

    Add files via upload

---
+++ SECOND.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int a,b,c,d;
+void main()
+{
+clrscr();
+printf("Enter the value of a: ");
+scanf("%d",&a);
+printf("Enter the value of b: ");
+scanf("%d",&b);
+printf("The value of a is %d and b is %d",a,b);
+c=a+b;
+printf("\nThe sum is %d ",c);
+}
