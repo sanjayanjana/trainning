80edf98c66378e3fafb58b31cce91b2fe793ecb9
sanjayanjana <93249310+sanjayanjana@users.noreply.github.com>
2021-10-28 15:09:13 +0530

    Add files via upload

---
+++ EXAMPLE5.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int fun(int a, int b)
+{
+	if(a==0||b==0)
+	{
+	return a;
+	}
+	if(b%2==0)
+	{
+	fun(a/2,b/3);
+	}
+	return fun(a+1,b-1);
+}
+   int main()
+
+   {
+	int x=12,y=10;
+	int z = fun(x,y);
+	printf("%d",z);
+	return 0;
+   }
