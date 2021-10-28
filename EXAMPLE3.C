261e2eaa4d8f0f73c008b49f0a44be536f68bd52
sanjayanjana <93249310+sanjayanjana@users.noreply.github.com>
2021-10-28 14:28:23 +0530

    Add files via upload

---
+++ EXAMPLE3.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	int j,i;
+	int a[]={5,6,1,-3,2,-2,1,0};
+	int N=sizeof(a)/sizeof(a[0]);
+	printf("%d",N);
+
+	for(i=2;i<3*N/3;i++)
+	{
+	  a[i]=a[i]*2;
+	  i++;
+	}
+	N++;
+	for(j=3*N/5;j>N%2;j--)
+	{
+	   int i=j/2;
+	   a[i]=a[i]-1;
+	   printf("%d",a[i]);
+	}
+	{
+	getch();
+	}
+	return 0;
+}
+
