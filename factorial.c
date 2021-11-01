82ace27250b2b293c3d5053f63b345da87334d23
sanjayanjana <93249310+sanjayanjana@users.noreply.github.com>
2021-11-01 12:00:05 +0530

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+
+int main()
+
+{
+    int i,n,fact=1;
+  
+  scanf("%d",&n);
+   
+ if(n>0)
+  
+  {
+     
+   printf("factorial");
+   
+ for(i=1;i<=n;i++)
+   
+   
+ {
+   
+ fact=fact*i;
+  
+  }
+
+printf("\nfactorial of %d is:%d",n,fact);
+
+}
+
+else
+
+{
+   
+ printf("not a factorial");
+
+}
+
+return 0;
+
+    
+}
+
+
